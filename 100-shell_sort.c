8f51871168f78e825a6f5e5e30cd08c988cad5fe
ALI <alierrami2024@gmail.com>
2023-10-28 16:46:46 -0700

    Sorting algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,23 +1,48 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "sort.h"
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void print_array(const int *array, size_t size)
+void swap_ints(int *a, int *b)
 {
-	size_t i;
+	int tmp;
 
-	i = 0;
-	while (array && i < size)
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
 	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
 	}
-	printf("\n");
 }
